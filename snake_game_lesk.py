ec83120562e70b465600783150d7ce9a1a0f1693
Calebe <calebemiranda2009@gmail.com>
2025-11-20 01:02:25 -0300

    refactor: improve snake_game_lesk.py

--- snake_game_lesk.py
+++ snake_game_lesk.py
@@ -3,7 +3,7 @@ import math
 import random
 import pygame
 
-BLOCO = 32 # 32
+BLOCO = 36 # 32
 TELA_LARGURA = BLOCO * 15 # 15
 TELA_COMPRIMENTO = BLOCO * 15 # 15
 
@@ -19,7 +19,7 @@ CORES = {
    "tela_cor": (11, 20, 59),
    "cobra_cor": (10, 200, 100),
    "moeda_cor": (143, 181, 211),
-   "inimigo_cor": (174, 0, 128),
+   "inimigo_cor": (219, 29, 89),
 }
 
 #//
@@ -32,23 +32,18 @@ class Cobra:
       self.corpo = pygame.Rect(self.inicio_x, self.inicio_y, BLOCO, BLOCO)
       self.velocidade = list(CIMA)
 
-      self.tela_largura = TELA_LARGURA
-      self.tela_comprimento = TELA_COMPRIMENTO
-
-      self.obstaculo = Obstaculo()
-
-   def mover(self):
+   def mover_cobra(self):
       self.corpo.x += self.velocidade[0]
       self.corpo.y += self.velocidade[1]
 
-   def colidiu(self):
+   def colidiu_borda_tela(self):
       if (self.corpo.x + BLOCO > TELA_LARGURA or 
           self.corpo.y + BLOCO > TELA_COMPRIMENTO or 
           self.corpo.x < 0 or 
           self.corpo.y < 0):
          return True
 
-   def direcao(self, direcao_nova):
+   def direcao_cobra(self, direcao_nova):
       if not (self.velocidade[0] != 0 and direcao_nova[0] != 0 or
               self.velocidade[1] != 0 and direcao_nova[1] != 0):
          self.velocidade = list(direcao_nova)
@@ -58,23 +53,80 @@ class Cobra:
    
 #//
 
-DISTANCIA_MINIMA = 45 # 40
+class Moeda:
+   def __init__(self, posicao):
+      self.tamanho_padrao = BLOCO - 16 # 36 - 15 = 20
+      self.tamanho_atual = 5
+      self.fase = "crescendo"
 
-QUANTIDADE_MOEDAS = 6 # 6
+      self.centro_xy = (posicao[0], posicao[1])
+      self.rect = pygame.Rect(posicao, [self.tamanho_atual, self.tamanho_atual])
 
-QUANTIDADE_POSICOES = QUANTIDADE_MOEDAS * 4
+   def atualizar_animacao(self):
+      velocidade = 2
+
+      if self.fase == "crescendo":
+         self.tamanho_atual += velocidade
+         if self.tamanho_atual >= self.tamanho_padrao * 1.4: # 28
+            self.fase = "encolhendo"
+      
+      if self.fase == "encolhendo":
+         self.tamanho_atual -= velocidade
+         if self.tamanho_atual <= self.tamanho_padrao:
+            self.tamanho_atual = self.tamanho_padrao
+            self.fase = "completo"
+      
+      self.rect.size = (self.tamanho_atual, self.tamanho_atual) 
+      self.rect.center = (self.centro_xy)
+
+#//
+
+class Inimigo:
+   def __init__(self, posicao):
+      self.tamanho_padrao = BLOCO - 18 # 36 - 18 = 18
+      self.tamanho_atual = [28, 26]
+      self.fase = "encolhendo"
+
+      self.centro_xy = (posicao[0], posicao[1])
+      self.rect = pygame.Rect(posicao, self.tamanho_atual)
+
+   def atualizar_animacao(self):
+      velocidade = 1
+
+      if self.fase == "encolhendo":
+         self.tamanho_atual[0] -= velocidade
+         self.tamanho_atual[1] -= velocidade
+         if self.tamanho_atual[0] <= self.tamanho_padrao / 1.6:
+            self.fase = "crescendo"
+      
+      if self.fase == "crescendo":
+         self.tamanho_atual[0] += velocidade
+         self.tamanho_atual[1] += velocidade
+         if self.tamanho_atual[0] >= self.tamanho_padrao:
+            self.tamanho_atual[1] = self.tamanho_padrao
+            self.tamanho_atual[0] = self.tamanho_padrao
+            self.fase = "completo"
+      
+      self.rect.size = (self.tamanho_atual[0], self.tamanho_atual[1])
+      self.rect.center = (self.centro_xy)
+
+#//
 
 class Obstaculo:
    def __init__(self):
-      self.posicoes_feitas = [] # LIMITE DE {QUANTIDADE_POSICOES} POSIÇÕES
+      self.distancia_minima = 45
+
+      self.QUANTIDADE_INIMIGOS = 6
+      self.inimigos_objeto = []
       
-      self.moedas_feitas = []
-      self.QUANTIDADE_INIMIGOS = 6 # 4
-      self.inimigos_feitos = []
+      self.QUANTIDADE_MOEDAS = 12
+      self.moedas_objeto = []
+      
+      self.quantidade_posicoes = self.QUANTIDADE_MOEDAS * 4
+      self.posicoes_feitas = []
 
    def criar_posicoes(self):
-
-      while len(self.posicoes_feitas) < QUANTIDADE_POSICOES:
+      while len(self.posicoes_feitas) < self.quantidade_posicoes:
          procurar = True
          
          while procurar:
@@ -86,30 +138,30 @@ class Obstaculo:
             for (x, y) in self.posicoes_feitas:
                distancia_entre_blocos = math.sqrt((x - pos_x)**2 + (y - pos_y)**2)
                
-               if not (distancia_entre_blocos > DISTANCIA_MINIMA):
+               if not (distancia_entre_blocos > self.distancia_minima):
                   procurar = True
                   break
          self.posicoes_feitas.append((pos_x, pos_y))      
 
-   def criar_obstaculos(self):
+   def criar_objeto_obstaculo(self):
       for posicao in self.posicoes_feitas[:]:
-         if len(self.moedas_feitas) < QUANTIDADE_MOEDAS:
-            moeda = pygame.Rect(posicao, (BLOCO - 15, BLOCO - 15))
-            self.moedas_feitas.append(moeda)
-            self.posicoes_feitas.remove(posicao)
-            continue
          
-         if len(self.inimigos_feitos) < self.QUANTIDADE_INIMIGOS:
-            inimigo = pygame.Rect(posicao, (BLOCO - 20 , BLOCO - 20))
-            self.inimigos_feitos.append(inimigo)
+         if len(self.moedas_objeto) < self.QUANTIDADE_MOEDAS:
+            moeda = Moeda(posicao)
+            self.moedas_objeto.append(moeda)
+            self.posicoes_feitas.remove(posicao)
+         
+         elif len(self.inimigos_objeto) < self.QUANTIDADE_INIMIGOS:
+            inimigo = Inimigo(posicao)
+            self.inimigos_objeto.append(inimigo)
             self.posicoes_feitas.remove(posicao)
 
-   def desenhar_obstaculos(self, tela):
-      for moeda in self.moedas_feitas:
-         pygame.draw.rect(tela, CORES["moeda_cor"], moeda)
+   def desenhar_rect_obstaculo(self, tela):
+      for moeda in self.moedas_objeto:
+         pygame.draw.rect(tela, CORES["moeda_cor"], moeda.rect)
       
-      for inimigo in self.inimigos_feitos:
-         pygame.draw.rect(tela, CORES["inimigo_cor"], inimigo)
+      for inimigo in self.inimigos_objeto:
+         pygame.draw.rect(tela, CORES["inimigo_cor"], inimigo.rect)
    
 #//
 
@@ -120,14 +172,11 @@ class Jogo:
       self.cobra = Cobra()
       self.obstaculo = Obstaculo()
       
-      self.titulo = pygame.display.set_caption("Lesk TIME!")
+      self.titulo = pygame.display.set_caption("A hora do Lesk!")
       self.time = pygame.time.Clock()
       self.rodando = True
 
-      self.variar_tamanho_tela()
-   
-   def variar_tamanho_tela(self):
-      self.tela = pygame.display.set_mode((self.cobra.tela_largura, self.cobra.tela_comprimento))
+      self.tela = pygame.display.set_mode((TELA_LARGURA, TELA_COMPRIMENTO))
 
    def processar_eventos(self):
       for evento in pygame.event.get():
@@ -139,47 +188,55 @@ class Jogo:
    
    def processar_teclas(self, tecla):
       if tecla == pygame.K_RIGHT:
-         self.cobra.direcao(DIREITA)
+         self.cobra.direcao_cobra(DIREITA)
       if tecla == pygame.K_UP:
-         self.cobra.direcao(CIMA)
+         self.cobra.direcao_cobra(CIMA)
       if tecla == pygame.K_LEFT:
-         self.cobra.direcao(ESQUERDA)
+         self.cobra.direcao_cobra(ESQUERDA)
       if tecla == pygame.K_DOWN:
-         self.cobra.direcao(BAIXO)
+         self.cobra.direcao_cobra(BAIXO)
+
+   def acao_obstaculos(self):
+      for moeda in self.obstaculo.moedas_objeto[:]:
+         
+         if moeda.fase != "completo":
+            moeda.atualizar_animacao()
+
+         if self.cobra.corpo.colliderect(moeda.rect):
+            self.obstaculo.moedas_objeto.remove(moeda)
+            print("MOEDA")
+
+            if random.random() < 0.8 and self.obstaculo.inimigos_objeto:
+               self.obstaculo.inimigos_objeto.pop(random.randint(0, len(self.obstaculo.inimigos_objeto) - 1))
+      
+      for inimigo in self.obstaculo.inimigos_objeto[:]:
+
+         if inimigo.fase != "completo":
+            inimigo.atualizar_animacao()
+
+         if self.cobra.corpo.colliderect(inimigo.rect):
+            self.obstaculo.inimigos_objeto.remove(inimigo)
+            print("MORTE...")
+   
+   #TODO: Usar a função de variar tela... nao consegui. Aprimorar aleatoriedade do surgimento dos obstaculos... Aprimorar a remoção? Coloca Menu, powerups, o que vai acontecer se eu pegar 15 moedas? e se eu pegar as inimigas? aleatoriedade e divertido
    
    def atualizar(self):
-      if len(self.obstaculo.posicoes_feitas) < QUANTIDADE_POSICOES:
-         self.obstaculo.criar_posicoes()
       
-      #TODO: transformar em uma função TUDO isso -> ... Usar a função de variar tela... Aprimorar aleatoriedade do surgimento dos obstaculos... Aprimorar a remoção?
+      self.obstaculo.criar_posicoes()
 
-      for moeda in self.obstaculo.moedas_feitas:
-         if self.cobra.corpo.colliderect(moeda):
-               print("moedadadada")
-               self.obstaculo.moedas_feitas.remove(moeda)
-
-               if random.random() < 0.6:
-                  self.obstaculo.inimigos_feitos.pop(random.randint(0, self.obstaculo.QUANTIDADE_INIMIGOS - 1))
-               # self.variar_tamanho_tela()
-      
-      for inimigo in self.obstaculo.inimigos_feitos:
-         if self.cobra.corpo.colliderect(inimigo):
-            self.obstaculo.inimigos_feitos.remove(inimigo)
-            
-            if random.random() < 0.6:
-               self.obstaculo.inimigos_feitos.pop(random.randint(0, self.obstaculo.QUANTIDADE_INIMIGOS - 1))
-      
-      self.obstaculo.criar_obstaculos()
-
+      self.obstaculo.criar_objeto_obstaculo()
    
-      self.cobra.mover()
-      if self.cobra.colidiu():
+      self.acao_obstaculos()
+   
+      self.cobra.mover_cobra()
+      if self.cobra.colidiu_borda_tela():
          self.rodando = False
    
    def desenhar_tudo(self):
+      
       self.tela.fill(CORES["tela_cor"])
       self.cobra.desenhar_cobra(self.tela)
-      self.obstaculo.desenhar_obstaculos(self.tela)
+      self.obstaculo.desenhar_rect_obstaculo(self.tela)
       
       pygame.display.flip()  
    
